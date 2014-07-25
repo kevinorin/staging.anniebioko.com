7efc75f43e797cbbd8d851f4f18651acfc636224
avistengg <avistcengg@gmail.com>
2014-07-25 11:29:46 -0700

    disable mage flag
    disable mage flag

--- index.php
+++ index.php
@@ -60,11 +60,11 @@ if (!file_exists($mageFilename)) {
     }
     exit;
 }
-
+/*
 if (file_exists($maintenanceFile)) {
     include_once dirname(__FILE__) . '/errors/503.php';
     exit;
-}
+}*/
 
 require_once $mageFilename;
 
